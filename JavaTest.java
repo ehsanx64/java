fcc5579f480dc0228b07d4113a4c865e9cd9fbf1
Ehsan Mohammadi <ehsanx64@gmail.com>
2019-01-30 13:27:43 +0330

    Refactored HelloWorld into its own package

--- JavaTest.java
+++ JavaTest.java
@@ -1,8 +1,13 @@
 // All test classes are put into this package, so we need to import
-import javatest.*;
+import javatest.helloworld.*;
 
 public class JavaTest {
 	public static void main (String[] args) {
+		JavaTest javatest = new JavaTest();
+		javatest.testHelloWorld();
+	}
+
+	private void testHelloWorld() {
 		// Instantiate and call a class method
 		HelloWorld hw = new HelloWorld();
 		Integer repeats = 5;
--- javatest/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-// This source file belongs to the following package
-package javatest;
-
-public class HelloWorld {
-	public void say() {
-		System.out.println("Hello world!!!");
-	}
-}
-
